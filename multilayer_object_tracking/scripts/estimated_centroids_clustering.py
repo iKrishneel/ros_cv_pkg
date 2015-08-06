726253c1e3a1d4bf24ed6263bba97c06de07474c
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2015-08-06 19:14:20 +0900

    clustering python script setup and changes to the srv file to handle other attributes of labels

--- multilayer_object_tracking/scripts/estimated_centroids_clustering.py
+++ multilayer_object_tracking/scripts/estimated_centroids_clustering.py
@@ -10,23 +10,29 @@ from sklearn import metrics
 
 def convert_pose_to_array(centroids):
     datapoints = []
-    for center in range(len(centroids)):
-        pos = [center.position.x, center.position.y, center.position.z]
+    for i in range(len(centroids)):
+        pos = [centroids[i].position.x,
+               centroids[i].position.y, 
+               centroids[i].position.z]
         datapoints.append(pos)
-    return datapoints
+    return np.array(datapoints)
 
 def run_clustering(centroids, max_distance, min_sample):
     datapoints = convert_pose_to_array(centroids)
+    
+    print datapoints
+    print "Input Points ", datapoints.shape
+
     db = DBSCAN(eps=max_distance, min_samples=min_sample).fit(datapoints)
-    """
-    complete this
-    """
+    labels = db.labels_
+    label, indices = np.unique(labels, return_inverse=True)
+    return (labels, indices, label)
 
 def estimated_centroids_clustering_handler(req):
-    labels = run_clustering(req.estimated_centroids,
-                           req.max_distance,
-                           req.min_samples)
-    return EstimatedCentroidClusteringResponse(labels)
+    labels, indices, elements = run_clustering(
+        req.estimated_centroids, req.max_distance, req.min_samples)
+    print labels
+    return EstimatedCentroidsClusteringResponse(labels, indices, elements)
 
 def estimated_centroid_clustering_server():
     rospy.init_node('estimated_centroids_clustering_server')
