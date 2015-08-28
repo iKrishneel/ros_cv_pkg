692e6b361a05b689f74bcf9d284ed1cb873dbe41
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2015-08-28 13:28:19 +0900

    typo error removed

--- multilayer_object_tracking/src/multilayer_object_tracking_node.cpp
+++ multilayer_object_tracking/src/multilayer_object_tracking_node.cpp
@@ -8,7 +8,7 @@ MultilayerObjectTracking::MultilayerObjectTracking() :
     init_counter_(0),
     update_counter_(0) {
     this->object_reference_ = ModelsPtr(new Models);
-    this->clustering_hhclient_ = this->pnh_.serviceClient<
+    this->clustering_client_ = this->pnh_.serviceClient<
        multilayer_object_tracking::EstimatedCentroidsClustering>(
           "estimated_centroids_clustering");
     this->onInit();
