4f18cd25de6fe8cfd7c96f29b03cde8359fcd10b
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2015-12-06 21:28:34 +0900

    fixed the previous crash bug

--- point_cloud_mincut_maxflow/src/point_cloud_mincut_maxflow_node.cpp
+++ point_cloud_mincut_maxflow/src/point_cloud_mincut_maxflow_node.cpp
@@ -60,32 +60,50 @@ void PointCloudMinCutMaxFlow::callback(
     std::cout << "DOING \t" << cloud->size() << std::endl;
     std::cout << num_threads_ << "\t" << neigbour_size_ << "\t"
               << is_search_ << std::endl;
+
     
-    
-    GraphPtr graph = GraphPtr(new Graph);
-    // this->graph_ = GraphPtr(new Graph);
-    this->makeAdjacencyGraph(graph, cloud, mask_cloud);
+    this->graph_ = GraphPtr(new Graph);
+    this->makeAdjacencyGraph(cloud, mask_cloud);
 
     std::cout << "\033[34m Graph Build \033[0m" << std::endl;
     
     ResidualCapacityMap residual_capacity = boost::get(
-       boost::edge_residual_capacity, *graph);
+       boost::edge_residual_capacity, *graph_);
 
     std::cout << "\033[34m Capacity Done \033[0m" << std::endl;
     std::cout << "S-T Info: " << source_ << "\t"
               << sink_ << std::endl;
-    std::cout << "Graph Info: " << boost::num_edges(*graph) << "\t"
-              << boost::num_vertices(*graph)<< std::endl;
+    std::cout << "Graph Info: " << boost::num_edges(*graph_) << "\t"
+              << boost::num_vertices(*graph_)<< std::endl;
+
+
+    std::cout << "c flow values:" << std::endl;
+    boost::property_map < Graph, boost::edge_capacity_t >::type
+       capacity = boost::get(boost::edge_capacity, *graph_);
+    boost::graph_traits < Graph >::vertex_iterator u_iter, u_end;
+    boost::graph_traits < Graph >::out_edge_iterator ei, e_end;
+    for (boost::tie(u_iter, u_end) = boost::vertices(*graph_);
+         u_iter != u_end; ++u_iter) {
+       for (boost::tie(ei, e_end) = boost::out_edges(*u_iter, *graph_);
+            ei != e_end; ++ei) {
+          // if (capacity[*ei] > 0) {
+             // std::cout << "Capacity: " << capacity[*ei] << std::endl;
+          
+             std::cout << "f " << *u_iter << " " << target(*ei, *graph_) << " "
+                       << (capacity[*ei] - residual_capacity[*ei])
+                       << std::endl;
+             // }
+       }
+    }
+
     
-    
-    double max_flow = boost::boykov_kolmogorov_max_flow(
-       *graph, this->source_, this->sink_);
+       double max_flow = boost::boykov_kolmogorov_max_flow(
+       *graph_, this->source_, this->sink_);
     std::cout << "DONE: " << max_flow  << std::endl;
     
 }
 
 void PointCloudMinCutMaxFlow::makeAdjacencyGraph(
-    GraphPtr graph,
     const pcl::PointCloud<PointT>::Ptr cloud,
     const pcl::PointCloud<PointT>::Ptr mask_cloud) {
     if (cloud->empty() || mask_cloud->empty()) {
@@ -93,49 +111,67 @@ void PointCloudMinCutMaxFlow::makeAdjacencyGraph(
        return;
     }
     const int num_size = cloud->size() + 2;
+
+    this->capacity_.reset();
+    this->capacity_ = boost::shared_ptr<CapacityMap>(new CapacityMap ());
+    *capacity_ = boost::get(boost::edge_capacity, *graph_);
+
+    reverse_edges_.reset();
+    reverse_edges_ = boost::shared_ptr<ReverseEdgeMap>(new ReverseEdgeMap ());
+    *reverse_edges_ = boost::get(boost::edge_reverse, *graph_);
+    
     VertexDescriptor vertex_descriptor(0);
     this->vertices_.clear();
     this->vertices_.resize(num_size, vertex_descriptor);
+    
     std::set<int> out_edges_marker;
     this->edge_marker_.clear();
     this->edge_marker_.resize(num_size, out_edges_marker);
-
+    
     // TODO(HERE):  Make parallel
     for (int i = 0; i < num_size; i++) {
-       this->vertices_[i] = boost::add_vertex(*graph);
+       this->vertices_[i] = boost::add_vertex(*graph_);
     }
-
+    
     this->source_ = this->vertices_[cloud->size()];
     this->sink_ = this->vertices_[cloud->size() + sizeof(char)];
 
-    icounter = 0;
-    
-    //! get neigbours and add edge
-    std::vector<float> neigbour_weights;
-    if (this->is_search_) {
-       bool is_search_ok = this->nearestNeigbourSearch(
-          graph, mask_cloud, this->neigbour_size_, neigbour_weights);
-       if (!is_search_ok || neigbour_weights.empty()) {
-          ROS_ERROR("ERROR: NEIGBHOUR SEARCH FAILED");
-          return;
-       }
-    }
 
     // TODO(HERE):  Make parallel
     for (int i = 0; i < cloud->size(); i++) {
        float src_weight = 0.80f;
        // float sink_weight = neigbour_weights[i];
-       float sink_weight = 0.50f;
-       this->addEdgeToGraph(graph, static_cast<int>(source_), i, src_weight);
-       // this->addEdgeToGraph(graph, i, static_cast<int>(source_), src_weight);
-       this->addEdgeToGraph(graph, static_cast<int>(sink_), i, sink_weight);
-       // this->addEdgeToGraph(graph, i, static_cast<int>(sink_), sink_weight);
+       float sink_weight = 23.50f;
+       
+       this->addEdgeToGraph(static_cast<int>(source_), i, src_weight);
+       this->addEdgeToGraph(i, static_cast<int>(sink_), sink_weight);
     }
 
+    std::cout << "TMP GRAPH SIZE 1: " <<
+       boost::num_edges(*graph_) << "\t"
+              << boost::num_vertices(*graph_) << std::endl;
+    
+    
+    
+    //! get neigbours and add edge
+    std::vector<float> neigbour_weights;
+    // if (this->is_search_) {
+       bool is_search_ok = this->nearestNeigbourSearch(
+          mask_cloud, this->neigbour_size_, neigbour_weights);
+       if (!is_search_ok || neigbour_weights.empty()) {
+          ROS_ERROR("ERROR: NEIGBHOUR SEARCH FAILED");
+          return;
+       }
+       // }
+
+      
+    std::cout << "TMP GRAPH SIZE: " <<
+       boost::num_edges(*graph_) << "\t"
+              << boost::num_vertices(*graph_) << std::endl;
 }
 
 bool PointCloudMinCutMaxFlow::nearestNeigbourSearch(
-    GraphPtr graph, const pcl::PointCloud<PointT>::Ptr cloud,
+    const pcl::PointCloud<PointT>::Ptr cloud,
     const int knearest, std::vector<float> &neigbour_weights) {
     if (cloud->empty()) {
        ROS_ERROR("ERROR: EMPTY POINT CLOUD FOR KDTREE");
@@ -144,8 +180,8 @@ bool PointCloudMinCutMaxFlow::nearestNeigbourSearch(
     pcl::KdTreeFLANN<PointT> kdtree;
     kdtree.setInputCloud(cloud);
     neigbour_weights.clear();
-    neigbour_weights.reserve(cloud->size());
-    float *nweight_ptr = &neigbour_weights[0];
+    // neigbour_weights.reserve(cloud->size());
+    // float *nweight_ptr = &neigbour_weights[0];
 // #ifdef _OPENMP
 // #pragma omp parallel for num_threads(this->num_threads_) shared(nweight_ptr)
 // #endif
@@ -155,36 +191,33 @@ bool PointCloudMinCutMaxFlow::nearestNeigbourSearch(
        PointT centroid_pt = cloud->points[i];
        if (!isnan(centroid_pt.x) || !isnan(centroid_pt.y) ||
            !isnan(centroid_pt.z)) {
-          int search_out = kdtree.nearestKSearch(i
-             /*centroid_pt*/, knearest, point_idx_search, point_squared_distance);
+          int search_out = kdtree.nearestKSearch(
+             centroid_pt, knearest, point_idx_search, point_squared_distance);
           float n_weights = 0.0f;
-
-          std::cout << i << "\t";
-          
           for (int j = 1; j < point_idx_search.size(); j++) {
-
-             std::cout << point_idx_search[j] << ", ";
-             
-             
              // weight difference
              float weight = 1.0f - (
                 abs(cloud->points[i].r - cloud->points[j].r) / 255.0f);
-             this->addEdgeToGraph(graph, i, point_idx_search[j], weight);
-             this->addEdgeToGraph(graph, point_idx_search[j], i, weight);
+
+             if (isnan(weight)) {
+                std::cout << "IS NAN: " << i << "\t" << j << std::endl;
+                weight = 0.0f;
+             }
+             
+             this->addEdgeToGraph(i, point_idx_search[j], weight);
+             this->addEdgeToGraph(point_idx_search[j], i, weight);
              n_weights += weight;
           }
           n_weights /= static_cast<float>(point_idx_search.size());
           // nweight_ptr[i] = n_weights;
           neigbour_weights.push_back(n_weights);
-
-          std::cout  << std::endl;;
        }
     }
     return true;
 }
 
 void PointCloudMinCutMaxFlow::addEdgeToGraph(
-    GraphPtr graph, const int source, const int sink, const float weight) {
+    const int source, const int sink, const float weight) {
     std::set<int>::iterator iter = this->edge_marker_[source].find(sink);
     if (iter != this->edge_marker_[source].end()) {
        // ROS_WARN("ERROR: EDGE NOT ADDED, %d \t %d", source, sink);
@@ -192,25 +225,27 @@ void PointCloudMinCutMaxFlow::addEdgeToGraph(
     } else {
        // ROS_INFO("INFO: EDGE ADDED, %d \t %d", source, sink);
     }
-    EdgeDescriptor edge_des;
+    EdgeDescriptor edge_descriptor;
     EdgeDescriptor reverse_edge;
     bool edge_was_added;
     bool reverse_edge_was_added = true;
-    boost::tie(edge_des, edge_was_added) = boost::add_edge(
-       this->vertices_[source], this->vertices_[sink], *graph);
+    boost::tie(edge_descriptor, edge_was_added) = boost::add_edge(
+       this->vertices_[source], this->vertices_[sink], *graph_);
     boost::tie(reverse_edge, reverse_edge_was_added) = boost::add_edge(
-       this->vertices_[sink], this->vertices_[source], *graph);
+       this->vertices_[sink], this->vertices_[source], *graph_);
     if (!edge_was_added || !reverse_edge_was_added) {
        ROS_ERROR("ERROR: EDGE ADD FAILED");
        return;
     }
-
-    // TODO(HERE): add other holders if required #L465
+    (*capacity_)[edge_descriptor] = weight;
+    (*capacity_)[reverse_edge] = 0.0f;
+    (*reverse_edges_)[edge_descriptor] = reverse_edge;
+    (*reverse_edges_)[reverse_edge] = edge_descriptor;
     this->edge_marker_[source].insert(sink);
 }
 
 void PointCloudMinCutMaxFlow::assembleLabels(
-    std::vector<pcl::PointIndices> &clusters, GraphPtr graph,
+    std::vector<pcl::PointIndices> &clusters, const GraphPtr graph,
     const ResidualCapacityMap &residual_capacity,
     const pcl::PointCloud<PointT>::Ptr cloud, const float epsilon) {
     std::vector<int> labels;
@@ -224,7 +259,7 @@ void PointCloudMinCutMaxFlow::assembleLabels(
     OutEdgeIterator edge_iter;
     OutEdgeIterator edge_end;
     for (boost::tie(edge_iter, edge_end) = boost::out_edges(
-            this->source_, *graph); edge_iter != edge_end; edge_iter++) {
+            this->source_, *graph_); edge_iter != edge_end; edge_iter++) {
        if (labels[edge_iter->m_target] == 1) {
           if (residual_capacity[*edge_iter] > epsilon) {
              clusters[1].indices.push_back(static_cast<int>(
