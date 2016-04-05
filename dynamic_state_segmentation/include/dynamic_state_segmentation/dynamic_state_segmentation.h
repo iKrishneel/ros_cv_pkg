0e10153cb18c87cb149ea181dec567e0dc77c75a
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-04-05 13:26:01 +0900

    recursive growing with parallel

--- dynamic_state_segmentation/include/dynamic_state_segmentation/dynamic_state_segmentation.h
+++ dynamic_state_segmentation/include/dynamic_state_segmentation/dynamic_state_segmentation.h
@@ -9,12 +9,6 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <sensor_msgs/image_encodings.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <cv_bridge/cv_bridge.h>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <boost/thread/mutex.hpp>
@@ -30,28 +24,64 @@
 #include <pcl/kdtree/impl/kdtree_flann.hpp>
 #include <pcl/common/transforms.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/registration/distances.h>
+
+#include <geometry_msgs/PointStamped.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <cv_bridge/cv_bridge.h>
 
 class DynamicStateSegmentation {
+
   typedef pcl::PointXYZRGB PointT;
- private:
-  boost::mutex mutex_;
-  ros::NodeHandle pnh_;
+  typedef pcl::Normal NormalT;
+  
+  private:
+   boost::mutex mutex_;
+   ros::NodeHandle pnh_;
   
   typedef  message_filters::sync_policies::ApproximateTime<
     sensor_msgs::PointCloud2,
-    sensor_msgs::PointCloud2> SyncPolicy;
-  // message_filters::Subscriber<sensor_msgs::PointCloud2> sub_cloud_;
-  // boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> >sync_;
+    geometry_msgs::PointStamped> SyncPolicy;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> sub_cloud_;
+  message_filters::Subscriber<geometry_msgs::PointStamped> screen_pt_;
+  boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> >sync_;
+
+  PointT seed_point_;
+  NormalT seed_normal_;
+  int seed_index_;
+
+  int num_threads_;
+  int icount_;
+  pcl::KdTreeFLANN<PointT> kdtree_;
   
  protected:
   void onInit();
   void subscribe();
   void unsubscribe();
+
+  ros::Publisher pub_cloud_;
   
  public:
-  DynamicStateSegmentation();
-  virtual void cloudCB(const sensor_msgs::PointCloud2::ConstPtr &);
   
+  DynamicStateSegmentation();
+  virtual void cloudCB(const sensor_msgs::PointCloud2::ConstPtr &,
+                       const geometry_msgs::PointStamped::ConstPtr &);
+  void seedCorrespondingRegion(std::vector<int> &, const pcl::PointCloud<PointT>::Ptr,
+                               const pcl::PointCloud<NormalT>::Ptr, const int);
+  void getPointNeigbour(std::vector<int> &, const pcl::PointCloud<PointT>::Ptr,
+                        const PointT, const int = 8);
+  int localVoxelConvexityCriteria(Eigen::Vector4f, Eigen::Vector4f, Eigen::Vector4f,
+                                  const float = 0.0f, bool = true);
+  template<class T>
+  void estimateNormals(const pcl::PointCloud<PointT>::Ptr,
+                       pcl::PointCloud<NormalT>::Ptr,
+                       T = 0.05f, bool = false) const;  
+
+  void computeFeatures(const PointT, const NormalT);
 };
 
 
--- dynamic_state_segmentation/src/dynamic_state_segmentation_node.cpp
+++ dynamic_state_segmentation/src/dynamic_state_segmentation_node.cpp
@@ -1,34 +1,213 @@
 
 #include <dynamic_state_segmentation/dynamic_state_segmentation.h>
 
-DynamicStateSegmentation::DynamicStateSegmentation() {
-
-  printf("HELLO\n");
+DynamicStateSegmentation::DynamicStateSegmentation() :
+    num_threads_(16) {
+    icount_ = 0;
+    this->onInit();
 }
 
 void DynamicStateSegmentation::onInit() {
-  
+    this->subscribe();
+
+    this->pub_cloud_ = this->pnh_.advertise<sensor_msgs::PointCloud2>(
+        "target", 1);
 }
 
 void DynamicStateSegmentation::subscribe() {
-  
+    this->sub_cloud_.subscribe(this->pnh_, "input_cloud", 1);
+    this->screen_pt_.subscribe(this->pnh_, "input_point", 1);
+    this->sync_ = boost::make_shared<message_filters::Synchronizer<
+                                       SyncPolicy> >(100);
+    this->sync_->connectInput(sub_cloud_, screen_pt_);
+    this->sync_->registerCallback(
+        boost::bind(&DynamicStateSegmentation::cloudCB, this, _1, _2));
 }
 
 void DynamicStateSegmentation::unsubscribe() {
-  
+    this->sub_cloud_.unsubscribe();
 }
 
 void DynamicStateSegmentation::cloudCB(
-    const sensor_msgs::PointCloud2::ConstPtr &cloud_msg) {
-  
+    const sensor_msgs::PointCloud2::ConstPtr &cloud_msg,
+    const geometry_msgs::PointStamped::ConstPtr &screen_msg) {
+    pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+    pcl::fromROSMsg(*cloud_msg, *cloud);
+
+    std::cout << "INPUT SIZE: " << cloud->size()  << "\n";
+    
+    if (cloud->empty()) {
+      return;
+    }
+
+    this->seed_index_ = screen_msg->point.x + (640 * screen_msg->point.y);
+    this->seed_point_ = cloud->points[seed_index_];
+
+    if (isnan(this->seed_point_.x) || isnan(this->seed_point_.x) ||
+        isnan(this->seed_point_.x)) {
+      ROS_ERROR("SELETED POINT IS NAN");
+      return;
+    }
+
+    std::vector<int> nan_indices;
+    pcl::removeNaNFromPointCloud<PointT>(*cloud, *cloud, nan_indices);
+
+    std::cout << "INPUT FILTERED SIZE: " << cloud->size()  << "\n";
+    
+    // temp update the seed based on removal shift
+    double dist = DBL_MAX;
+    int idx = -1;
+    for (int i = 0; i < cloud->size(); i++) {
+      double d = pcl::distances::l2(cloud->points[i].getVector4fMap(),
+                                    seed_point_.getVector4fMap());
+      if (d < dist) {
+        dist = d;
+        idx = i;
+      }
+    }
+    this->seed_index_ = idx;
+    this->seed_point_ = cloud->points[idx];
+    
+    ROS_INFO("PROCESSING");
+    
+    std::vector<int> labels(static_cast<int>(cloud->size()));
+    for (int i = 0; i < cloud->size(); i++) {
+      if (i == this->seed_index_) {
+        labels[i] = 1;
+      }
+      labels[i] = -1;
+    }
+
+    pcl::PointCloud<NormalT>::Ptr normals(new pcl::PointCloud<NormalT>);
+    this->estimateNormals<float>(cloud, normals, 0.03f);
+    kdtree_.setInputCloud(cloud);
+
+    ROS_INFO("GROWING SEED");
+    this->seedCorrespondingRegion(labels, cloud, normals, this->seed_index_);
+
+    pcl::PointCloud<PointT>::Ptr seed_region(new pcl::PointCloud<PointT>);
+    for (int i = 0; i < labels.size(); i++) {
+      if (labels[i] != -1) {
+        PointT pt = cloud->points[i];
+        seed_region->push_back(pt);
+      }
+    }
+    
+    ROS_INFO("DONE.");
+    
+    sensor_msgs::PointCloud2 ros_cloud;
+    pcl::toROSMsg(*seed_region, ros_cloud);
+    ros_cloud.header = cloud_msg->header;
+    this->pub_cloud_.publish(ros_cloud);
+}
+
+void DynamicStateSegmentation::seedCorrespondingRegion(
+    std::vector<int> &labels, const pcl::PointCloud<PointT>::Ptr cloud,
+    const pcl::PointCloud<NormalT>::Ptr normals, const int parent_index) {
+    std::vector<int> neigbor_indices;
+    this->getPointNeigbour(neigbor_indices, cloud, cloud->points[parent_index], 50);
+
+    int neigb_lenght = static_cast<int>(neigbor_indices.size());
+    std::vector<int> merge_list(neigb_lenght);
+    merge_list[0] = -1;
+
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(this->num_threads_) shared(merge_list, labels)
+#endif   
+    for (int i = 1; i < neigbor_indices.size(); i++) {
+      int index = neigbor_indices[i];
+      if (index != parent_index && labels[index] == -1) {
+        Eigen::Vector4f parent_pt = cloud->points[parent_index].getVector4fMap();
+        Eigen::Vector4f child_pt = cloud->points[index].getVector4fMap();
+        Eigen::Vector4f child_norm = normals->points[index].getNormalVector4fMap();
+        if (this->localVoxelConvexityCriteria(parent_pt, child_pt, child_norm, -0.005) == 1) {
+          // merge_list.push_back(index);
+          merge_list[i] = index;
+          labels[index] = 1;
+        } else {
+          merge_list[i] = -1;
+        }
+      } else {
+        merge_list[i] = -1;
+      }
+    }
+    
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(this->num_threads_)
+#endif   
+    for (int i = 0; i < merge_list.size(); i++) {
+      int index = merge_list[i];
+      if (index != -1) {
+        seedCorrespondingRegion(labels, cloud, normals, index);
+      }
+    }
+}
+
+void DynamicStateSegmentation::getPointNeigbour(
+    std::vector<int> &neigbor_indices, const pcl::PointCloud<PointT>::Ptr cloud,
+    const PointT seed_point, const int K) {
+    if (cloud->empty() || isnan(seed_point.x) ||
+      isnan(seed_point.y) || isnan(seed_point.z)) {
+      ROS_ERROR("THE CLOUD IS EMPTY. RETURING VOID IN GET NEIGBOUR");
+      return;
+    }
+    neigbor_indices.clear();
+    std::vector<float> point_squared_distance;
+    //int search_out = kdtree_.nearestKSearch(
+    // seed_point, K, neigbor_indices, point_squared_distance);
+    int search_out = kdtree_.radiusSearch(
+        seed_point, 0.01f, neigbor_indices, point_squared_distance);
+}
+
+int DynamicStateSegmentation::localVoxelConvexityCriteria(
+    Eigen::Vector4f c_centroid, Eigen::Vector4f n_centroid,
+    Eigen::Vector4f n_normal, const float thresh, bool is_seed) {
+    float im_relation = (n_centroid - c_centroid).dot(n_normal);
+    float seed_relation = FLT_MAX;
+    if (is_seed) {
+      seed_relation = (n_centroid - this->seed_point_.getVector4fMap()).dot(n_normal);
+    }
+    if (im_relation > thresh && seed_relation > thresh) {
+      return 1;
+    } else {
+      return -1;
+    }
+}
+
+template<class T>
+void DynamicStateSegmentation::estimateNormals(
+    const pcl::PointCloud<PointT>::Ptr cloud, pcl::PointCloud<NormalT>::Ptr normals,
+    const T k, bool use_knn) const {
+    if (cloud->empty()) {
+      ROS_ERROR("ERROR: The Input cloud is Empty.....");
+      return; 
+    }
+    pcl::NormalEstimationOMP<PointT, NormalT> ne;
+    ne.setInputCloud(cloud);
+    ne.setNumberOfThreads(this->num_threads_);
+    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+    ne.setSearchMethod(tree);
+    if (use_knn) {
+      ne.setKSearch(k);
+    } else {
+      ne.setRadiusSearch(k);
+    }
+    ne.compute(*normals);
+}
+
+void DynamicStateSegmentation::computeFeatures(
+    const PointT point, const NormalT normal) {
+    if (isnan(point.x) || isnan(point.y) || isnan(point.z)) {
+      ROS_ERROR("THE CLOUD IS EMPTY. RETURING VOID IN FEATURES");
+      return;
+    }
+    // TODO: COMPUTE FEATURES IF REQUIRED
 }
 
 int main(int argc, char *argv[]) {
-  ros::init(argc, argv, " dynamic_state_segmentation");
+  ros::init(argc, argv, "dynamic_state_segmentation");
   DynamicStateSegmentation dss;
   ros::spin();
-
-  printf("HELLO\n");
   return 0;
 }
 
